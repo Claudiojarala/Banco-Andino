28400604a6797c2dc6337ae7f7dd888600a37627
Claudiojarala <claudio.jara.lamas22@gmail.com>
2026-04-22 03:18:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 # CONFIGURACIÓN DE INFRAESTRUCTURA (CLOUD GOVERNMENT)
 # =========================================================
 load_dotenv()
-DB_URL = os.getenv("DATABASE_URL=postgresql://postgres.ayvcwlgaexdjlnuczycv:Ar8O9eR0avwn4wdS@aws-1-us-east-2.pooler.supabase.com:6543/postgres")
+DB_URL = os.getenv("DATABASE_URL")
 
 
 def get_db_connection():
